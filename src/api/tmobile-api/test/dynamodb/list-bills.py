187b84b76f4029a5be05bd49c99ad9cc0f04c39a
sairamaj <sairamaj@gmail.com>
2021-03-27 09:06:42 -0700

    adding bill details

---
+++ src/api/tmobile-api/test/dynamodb/list-bills.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+from boto3.dynamodb.conditions import Key
+
+# Get the service resource.
+dynamodb = boto3.resource(
+    'dynamodb',
+    endpoint_url='http://127.0.0.1:8000'
+)
+
+table = dynamodb.Table('TMobile')
+
+class Primary:
+    def __init__(self,primary):
+        self.primary = primary
+
+
+bills = table.query(
+    KeyConditionExpression=Key('Name').eq('Bills') & Key('Type').begins_with('Summary')
+)
+
+#print(users["Items"])
+print(bills['Items'])
---
+++ src/api/tmobile-api/test/dynamodb/uploadbill.py
@@ -0,0 +1,180 @@
+import glob
+import tabula
+import boto3
+import os
+from decimal import Decimal
+
+numberOfLines = 12
+
+def floatToDecimal(val):
+    return Decimal(format(val, ".15g"))
+
+class Entry:
+    def __init__(self, number, amount, equipment, services, onetime_charge, total):
+        self.number = number
+        self.amount = amount
+        self.equipment = equipment
+        self.onetime_charge = onetime_charge
+        self.services = services
+        self.total = total
+
+def toAmount(val):
+    if val.strip() == '-':
+        return 0.0
+    return float(val.lstrip('$'))
+
+
+
+class EntryParser:
+    def __init__(self, ds_columns):
+        self.phone = 0
+        self.equipment = -1.0
+        self.services = -1.0
+        self.onetime_charge = -1.0
+        print(f'type is:{type(ds_columns)}')
+        columns = ds_columns.to_list()
+        print(columns)
+        print(type(columns))
+        if "Equipment" in columns:
+            self.equipment = columns.index('Equipment')
+        if "Services" in columns:
+            self.services = columns.index('Services')
+        if "One-time charges" in columns:
+            self.onetime_charge = columns.index('One-time charges')
+
+    def parse(self, dFrame):
+        phone = dFrame[self.phone]
+        equipment = 0.0
+        services = 0.0
+        onetime_charge = 0.0
+        if self.equipment > 0:
+            equipment = toAmount(dFrame[self.equipment])
+        if self.services > 0:
+            services = toAmount(dFrame[self.services])
+        if self.onetime_charge > 0:
+            onetime_charge = toAmount(dFrame[self.onetime_charge])
+        total = self.perLine + equipment + services + onetime_charge
+        return Entry(phone, self.perLine, equipment, services, onetime_charge, total)
+
+
+def parseBill(fileName):
+    table = tabula.read_pdf(fileName, pages=2)
+    ds = table[0]
+
+    entryParser = EntryParser(ds.columns)
+
+    entries = []
+    row = 0
+    perLine = 0
+    planAmount = 0
+    finalAmount = 0
+    for val in ds.values:
+
+        # -----------------------row 0 -------------------------
+        # Line Type Plans Equipment Services Total
+        # -----------------------row 1-------------------------
+        # Totals $233.22 $29.17 $0.00 $262.39
+        # -----------------------row 2 onwards -------------------------
+        # (xxx) xxx-xxxx Voice $4.28 - - $4.28
+        if row == 0:
+            finalAmount = toAmount(val[-1])
+            planAmount = toAmount(val[2])
+            entryParser.perLine = planAmount/numberOfLines
+
+        if row >= 2:
+            entries.append(entryParser.parse(val))
+        row = row+1
+
+    return planAmount, finalAmount, entries
+
+
+def display(entries):
+    print(f"{'Number':^18}|{'Amount':^12}|{'Equipment':^12}|{'Services':^12}|{'One Time':^12}|{'Total':^12}")
+    for entry in entries:
+        print(
+            f"{entry.number:^18}|{entry.amount:^12.2f}|{entry.equipment:^12}|{entry.services:^12}|{entry.onetime_charge:^12}|{entry.total:^12.2f}")
+    print('______________________________________________________')
+
+
+def validate(file, billAmount, entires):
+    entriesTotal = 0
+    for entry in entries:
+        entriesTotal += entry.total
+
+    if(abs(billAmount - entriesTotal) > 1.0):
+        print('-------------- NO MATCH -----------------')
+        raise ValueError(
+            f"Not matched: {billAmount} with {entriesTotal} in {file}")
+    else:
+        print('-------------- MATCH -----------------')
+        print('________________________________________')
+        print(f'Bill Total: {billAmount:.2f}')
+        print(f'Total: from entries:{entriesTotal:.2f}')
+
+
+def save(month, entries):
+    file = f'{month}.txt'
+    with open(file, 'w') as writer:
+        writer.write(
+            f"{'Number':^18}|{'Amount':^12}|{'Equipment':^12}|{'Services':^12}|{'One Time':^12}|{'Total':^12}\n")
+        for entry in entries:
+            writer.write(
+                f"{entry.number:^18}|{entry.amount:^12.2f}|{entry.equipment:^12}|{entry.services:^12}|{entry.onetime_charge:^12}|{entry.total:^12.2f}\n")
+    print(f"{file} saved.")
+
+
+def upload(month, finalBill, perline, entries):
+
+    dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url='http://localhost:8000'
+    )
+
+    table = dynamodb.Table('TMobile')
+    print(f'bill month:{month}')
+    print(f'perline: {perline} : {type(perline)}')
+    print(f'finalBill: {finalBill} : {type(finalBill)}')
+    table.put_item(
+        Item={
+            'Name': 'Bills',
+            'Type': f'Summary_{month}',
+            'Total': floatToDecimal(finalBill),
+            "PerLine": floatToDecimal(perline)
+        }
+    )
+
+    # add all entries
+    for entry in entries:
+        table.put_item(
+            Item={
+                'Name': 'Bills',
+                'Type': f'Details_{month}_{entry.number}',
+                'Number': entry.number,
+                "PlanAmount" : floatToDecimal(entry.amount),
+                "Equipment": floatToDecimal(entry.equipment),
+                "Services": floatToDecimal(entry.services),
+                "OneTimeCharge" : floatToDecimal(entry.onetime_charge),
+                "Total" : floatToDecimal(entry.total)
+            }
+        )
+
+for file in glob.glob('c:\\sai\\dev\\temp\\pdf\\tmobile\\*.pdf'):
+    print(f' parsing : {file}')
+    planAmount, billAmount, entries = parseBill(file)
+    
+    # display to view.
+    display(entries)
+    print(f'final amount:{billAmount:.2f}')
+    print(f'plan Amount:{planAmount:.2f}')
+    
+    # validate the bill for amounts.
+    validate(file, billAmount, entries)
+    
+    # save for local testing
+    head, tail = os.path.split(file)
+    month = os.path.splitext(tail)[0]
+    month = month.lstrip('SummaryBill')
+    save(month, entries)
+    
+    # upload
+    upload(month, billAmount, planAmount, entries)
