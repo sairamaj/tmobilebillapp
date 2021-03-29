e10333c351299f345e71e6d665e508fbd6d32009
sairamaj <sairamaj@gmail.com>
2021-03-29 12:28:06 -0700

    adding authentiation

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -1,16 +1,12 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using MatBlazor;
-using web.Repository;
-using Microsoft.Extensions.Caching.Memory;
 using web.Shared;
+using web.Repository;
+using MatBlazor;
 using Blazored.LocalStorage;
 
 namespace web
@@ -23,13 +19,22 @@ namespace web
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddSingleton<IMemoryCache, MemoryCache>();
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+            });
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<ICacheManager, CacheManager>();
             builder.Services.AddScoped<IBillRepository, BillRepository>();
             //  builder.Services.AddScoped<IBillRepository, FakeBillRepository>();
             builder.Services.AddMatBlazor();
             builder.Services.AddBlazoredLocalStorage(config =>
                     config.JsonSerializerOptions.WriteIndented = true);
+
             await builder.Build().RunAsync();
         }
     }
