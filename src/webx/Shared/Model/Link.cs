926561aed041fddd0ae03e1f974ef5c42ae987d2
sairamaj <sairamaj@gmail.com>
2021-04-01 21:10:49 -0700

    adding dotnet core install

--- src/webx/Shared/Model/Link.cs
+++
@@ -1,7 +0,0 @@
-namespace web.Shared.Model
-{
-    public class Link
-    {
-        public string Url { get; set; }  
-    }
-}
--- src/webx/Shared/Model/User.cs
+++
@@ -1,9 +0,0 @@
-namespace web.Shared.Model
-{
-    internal class User
-    {
-        public string Name {get; set;}
-        public string Phone {get; set;}
-        public string Primary {get; set;}
-    }
-}
