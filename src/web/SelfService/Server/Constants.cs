386090a320b40b1d43e2d242ece2147dd68ca8b7
sairamaj <sairamaj@gmail.com>
2021-04-06 16:59:06 -0700

    reducing the cache

--- src/web/SelfService/Server/Constants.cs
+++ src/web/SelfService/Server/Constants.cs
@@ -7,7 +7,7 @@ namespace SelfService.Server
         public static TimeSpan UsersCacheExpiry = new TimeSpan(10, 0, 0);
         public static TimeSpan BillCacheExpiry = new TimeSpan(10, 0, 0);
         public static TimeSpan BillDetailsCacheExpiry = new TimeSpan(10, 0, 0);
-        public static TimeSpan DownloadUrlCacheExpiry = new TimeSpan(0, 45, 0);
+        public static TimeSpan DownloadUrlCacheExpiry = new TimeSpan(0, 5, 0);
         public static TimeSpan UserRolesCacheExpiry = new TimeSpan(0, 15, 0);
     }
 }
